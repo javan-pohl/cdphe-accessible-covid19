c9e42b900eab807c18121daaef8e1a815f8d1237
Josh Bernstein <joshua.bernstein@outlook.com>
2020-11-16 19:13:54 -0700

    Updated hook

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import Card from './components/Card/Card'
-import { API_URL } from './utils/constants';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Card from "./components/Card/Card";
+import { API_URL } from "./utils/constants";
 
 const cleanData = (data) => {
   const newData = [];
-  data.map(attr => newData.push(...Object.values(attr)));
-  return newData.filter(attr => attr.Date !== null);
-}
-
+  data.map((attr) => newData.push(...Object.values(attr)));
+  return newData.filter((attr) => attr.Date !== null);
+};
 
 const App = () => {
   const [data, setData] = useState([]);
+  console.log("Running App");
 
   useEffect(() => {
     fetch(API_URL)
-    .then(res => res.json())
-    .then(resJson => {
-      const cleanedData = cleanData(resJson.features);
-      setData(cleanedData);
-    }) 
-    .catch(err => console.log(err))
-  })
+      .then((res) => res.json())
+      .then((resJson) => {
+        const cleanedData = cleanData(resJson.features);
+        setData(cleanedData);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  console.log(data);
 
   return (
-    <div className="App">
-        <Card data={data}/>
+    <div className='App'>
+      <Card data={data} />
     </div>
   );
-}
+};
 
 export default App;
