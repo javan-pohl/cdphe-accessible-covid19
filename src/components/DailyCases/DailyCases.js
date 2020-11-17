de014649de9ab203c835bb96ef8be2dc82b0dfee
Katie Hansen <khansen0415@gmail.com>
2020-11-16 18:53:14 -0700

    update folder structure

---
+++ src/components/DailyCases/DailyCases.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import Card from './Card/Card'
+import { API_URL } from '../../utils/constants';
+
+const cleanData = (data) => {
+  const newData = [];
+  data.map(attr => newData.push(...Object.values(attr)));
+  return newData.filter(attr => attr.Date !== null);
+}
+
+
+const DailyCases = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+      fetch(API_URL)
+      .then(res => res.json())
+      .then(resJson => {
+        const cleanedData = cleanData(resJson.features);
+        setData(cleanedData);
+      }) 
+      .catch(err => console.log(err))
+    })
+
+  return (
+    <div>
+        <Card data={data}/>
+    </div>
+  );
+}
+
+export default DailyCases;
