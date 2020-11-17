1fccf144fc582b12b33000ab4733441671b3f5e1
Josh Bernstein <joshua.bernstein@outlook.com>
2020-11-16 20:26:40 -0700

    Added basic table with the latest meta data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import React from 'react';
-import DailyCases from './components/DailyCases/DailyCases';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import DailyCases from "./components/DailyCases/DailyCases";
+import DailyCasesTable from "./components/DailyCases/DailyCasesTable";
+import { API_URL } from "./utils/constants";
+import "./App.css";
+import "tabler-react/dist/Tabler.css";
 
 const App = () => {
+  const [data, setData] = useState([]);
+
+  const cleanData = (data) => {
+    const newData = [];
+    data.map((attr) => newData.push(...Object.values(attr)));
+    return newData.filter((attr) => attr.Date !== null);
+  };
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then((resJson) => {
+        const cleanedData = cleanData(resJson.features);
+        setData(cleanedData);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  let latest = {};
+  latest = data.slice(-1)[0];
+
   return (
-    <div className="App">
-        <DailyCases />
+    <div className='App'>
+      <DailyCases data={data} />
+      <DailyCasesTable data={latest} />
     </div>
   );
 };
--- src/components/DailyCases/DailyCases.js
+++ src/components/DailyCases/DailyCases.js
@@ -1,32 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import Card from './Card/Card'
-import { API_URL } from '../../utils/constants';
+import React, { useState, useEffect } from "react";
+import Card from "./Card/Card";
 
-const cleanData = (data) => {
-  const newData = [];
-  data.map(attr => newData.push(...Object.values(attr)));
-  return newData.filter(attr => attr.Date !== null);
-}
-
-
-const DailyCases = () => {
-    const [data, setData] = useState([]);
-
-    useEffect(() => {
-      fetch(API_URL)
-      .then(res => res.json())
-      .then(resJson => {
-        const cleanedData = cleanData(resJson.features);
-        setData(cleanedData);
-      }) 
-      .catch(err => console.log(err))
-    })
-
+const DailyCases = (props) => {
   return (
     <div>
-        <Card data={data}/>
+      <Card data={props.data} />
     </div>
   );
-}
+};
 
 export default DailyCases;
