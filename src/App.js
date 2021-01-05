45d36d0a5ed34c8d2d158a700d743da2c2aad42c
Josh Bernstein <joshua.bernstein@outlook.com>
2021-01-04 22:07:37 -0700

    Rebuilt stats card and built preliminary map of datapoints

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React, { useState, useEffect } from "react";
-import DailyStats from './components/DailyStats/DailyStats';
-import LatestSnapshot from './components/LatestSnapshot/LatestSnapshot';
+import DailyStats from "./components/DailyStats/DailyStats";
+import LatestSnapshot from "./components/LatestSnapshot/LatestSnapshot";
 import { API_URL } from "./utils/constants";
+
+import { Home } from "./pages/home";
 import "./App.css";
 import "tabler-react/dist/Tabler.css";
 
+import { Page } from "tabler-react";
+
 const App = () => {
   const [data, setData] = useState([]);
-
+  console.log(data);
   const cleanData = (data) => {
     const newData = [];
+
     data.map((attr) => newData.push(...Object.values(attr)));
+    //Need to add a sort here, it looks like 2021 is at the top, and 2020 is at the bottom
     return newData.filter((attr) => attr.Date !== null);
   };
 
@@ -25,30 +31,17 @@ const App = () => {
   }, []);
 
   return (
-    <div className="App">
-      <DailyStats
-        data={data}
-        type="Cases"
-        yAccessor="Cases"
-      />
-      <DailyStats
-        data={data}
-        type="Hospitalized"
-        yAccessor="Hosp"
-      />
-      <DailyStats
-        data={data}
-        type="Deaths"
-        yAccessor="Deaths"
-      />
-      <DailyStats
-        data={data}
-        type="Tested"
-        yAccessor="Tested"
-      />
-      <LatestSnapshot 
-        data={data[0]} 
-      /> 
+    <div className='App'>
+      <Page>
+        <Page.Content title='Covid 19 Dashboard'>
+          <Home today={data[0]} yesterday={data[1]} />
+          <DailyStats data={data} type='Cases' yAccessor='Cases' />
+          <DailyStats data={data} type='Hospitalized' yAccessor='Hosp' />
+          <DailyStats data={data} type='Deaths' yAccessor='Deaths' />
+          <DailyStats data={data} type='Tested' yAccessor='Tested' />
+          <LatestSnapshot data={data[0]} />
+        </Page.Content>
+      </Page>
     </div>
   );
 };
---
+++ src/pages/home.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Grid } from "tabler-react";
+import { StatsCard } from "../components/StatsCard/StatsCard";
+
+const GridContainer = (props) => (
+  <Grid.Col sm={6} md={4} lg={3}>
+    {props.children}
+  </Grid.Col>
+);
+
+export const Home = (props) => {
+  const { today, yesterday } = props;
+
+  return (
+    <div>
+      {today && yesterday && (
+        <Grid.Row cards deck>
+          {Object.keys(today).map((key) => (
+            <GridContainer>
+              <StatsCard
+                movement={today[key] - yesterday[key]}
+                movementType={" daily"}
+                total={today[key]}
+                label={key}
+              />
+            </GridContainer>
+          ))}
+        </Grid.Row>
+      )}
+    </div>
+  );
+};
