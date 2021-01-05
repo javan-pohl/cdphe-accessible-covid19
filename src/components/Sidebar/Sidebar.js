a08d7c02d8c2924c989220539c8cebf9a359ae7c
Joel Lacey <joellaceybusiness@gmail.com>
2021-01-05 18:47:43 -0500

    Add static section Sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,19 +1,30 @@
 import React from "react";
-import { Nav } from "tabler-react";
 import './Sidebar.css'
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
-    <Nav className="sidebar">
-        <h1 className="title">Dashboard</h1>
-        <Nav.Item hasSubNav value="Case Summary">
-            <Nav.SubItem value="Cases" />
-            <Nav.SubItem value="Tested" />
-            <Nav.SubItem value="Hospitalized" />
-            <Nav.SubItem value="Deaths" />
-        </Nav.Item>
-        <Nav.Item to="http://www.example.com">Daily Snapshot</Nav.Item>
-    </Nav>
+    <section className="sidebar">
+        <h1 className="title">Covid-19 Dashboard</h1>
+        <Link to="/">
+          <h4 className="option">Daily Snapshot</h4>
+        </Link>
+        <h4 className="option">Case Summary</h4>
+        <ul className="sub-list" value="Case Summary">
+          <Link to="/daily-cases">
+            <li className="sub-option">Cases</li>
+          </Link>
+          <Link to="/daily-tested">
+            <li className="sub-option">Tested</li>
+          </Link>
+          <Link to="/daily-hosp">
+            <li className="sub-option">Hospitalized</li>
+          </Link>
+          <Link to="/daily-deaths">
+            <li className="sub-option">Deaths</li>
+          </Link>
+        </ul>
+    </section>
   );
 };
 
