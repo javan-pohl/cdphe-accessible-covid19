3ad25c572afaa251bc328ec0b8715a2b19916507
Joel Lacey <joellaceybusiness@gmail.com>
2021-01-05 19:12:29 -0500

    Fix tabler table nesting warning

--- src/components/LatestSnapshot/LatestSnapshot.js
+++ src/components/LatestSnapshot/LatestSnapshot.js
@@ -1,27 +1,28 @@
 import React from "react";
 import { Table } from "tabler-react";
+import './LatestSnapshot.css'
 
-const DailySnapshot = (props) => {
+const LatestSnapshot = (props) => {
   const { data } = props; 
 
   return (
-    <div>
-      <Table>
-        <Table.Header>
+    <Table className="snapshot">
+      <Table.Header>
+        <tr>
           <Table.ColHeader>Date</Table.ColHeader>
           <Table.ColHeader>Daily Colorado Covid-19 Statistics</Table.ColHeader>
-        </Table.Header>
-        <Table.Body>
-          {data && Object.keys(data).map((key) => (
-            <Table.Row key={key}>
-              <Table.Col>{key}</Table.Col>
-              <Table.Col>{data[key]}</Table.Col>
-            </Table.Row>
-          ))}
-        </Table.Body>
-      </Table>
-    </div>
+        </tr>
+      </Table.Header>
+      <Table.Body>
+        {data && Object.keys(data).map((key) => (
+          <Table.Row key={key}>
+            <Table.Col>{key}</Table.Col>
+            <Table.Col>{data[key]}</Table.Col>
+          </Table.Row>
+        ))}
+      </Table.Body>
+    </Table>
   );
 };
 
-export default DailySnapshot;
+export default LatestSnapshot;
