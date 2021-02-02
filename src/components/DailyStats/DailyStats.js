3dca4e1d15f6320f903e277b0009d5c82d45827a
Joel Lacey <joellaceybusiness@gmail.com>
2021-02-02 15:55:44 -0500

    Reinstate DailyStat grouping

---
+++ src/components/DailyStats/DailyStats.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+import Graph from "./Graph/Graph";
+import Table from "./Table/Table";
+import { Button } from "tabler-react";
+
+
+const DailyStats = ({data, type, yAccessor}) => {
+  const [tableDisplay, setTableDisplay] = useState(false);
+
+  return (
+    <React.Fragment>
+    </React.Fragment>
+  );
+}
+
+export default DailyStats;
