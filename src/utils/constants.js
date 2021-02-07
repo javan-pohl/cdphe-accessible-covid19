f9c9c612800611b772c83bebd36e24edda48c82c
Joel Lacey <joellaceybusiness@gmail.com>
2021-02-07 17:04:24 -0500

    Update daily URL for route change

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 //Daily Statistics: https://data-cdphe.opendata.arcgis.com/datasets/cdphe-covid19-daily-state-statistics-1
 
 const API_URLS = {
-    dailyStatistics: 'https://services3.arcgis.com/66aUo8zsujfVXRIT/arcgis/rest/services/CDPHE_COVID19_Daily_State_Statistics/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json',
-    testPositivity: 'https://services3.arcgis.com/66aUo8zsujfVXRIT/arcgis/rest/services/COVID19_Positivity_Data_from_Clinical_Laboratories/FeatureServer/0/query',
+    dailyStatistics: 'https://services3.arcgis.com/66aUo8zsujfVXRIT/arcgis/rest/services/colorado_covid19_daily_state_statistics_cumulative/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json',
+    testPositivity: 'https://services3.arcgis.com/66aUo8zsujfVXRIT/arcgis/rest/services/colorado_covid19_laboratory_positivity_data/FeatureServer/0/query',
 }
 exports.API_URLS = API_URLS;
