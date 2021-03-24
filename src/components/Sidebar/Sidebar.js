eb7940198f9a0525d98dea3714da75b4f616d99d
Joel Lacey <joellaceybusiness@gmail.com>
2021-03-23 22:19:15 -0400

    Make menus more keyboard navigable

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -4,42 +4,42 @@ import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
-    <nav className="sidebar" role="menu" aria-label="Denver Covid-19 Visualization Options">
+    <nav className="sidebar" role="menubar" aria-label="Denver Covid-19 Visualization Options">
         <h1 className="title">Colorado Covid-19 Dashboard</h1>
-        <Link to="/" role="none">
-          <h4 className="option" role="menuitem">Daily Snapshot</h4>
+        <Link to="/" className="link">
+          <h4 className="option" role="menuitem" tabIndex="0">Daily Snapshot</h4>
         </Link>
         <h4 className="option">Case Summary</h4>
-        <ul className="sub-list" value="Case Summary" role="menuitem">
-          <Link to="/daily-cases">
-            <li className="sub-option" role="menuitem" aria-label="Daily Cases">Cases</li>
+        <ul className="sub-list" value="Case Summary" role="menu">
+          <Link to="/daily-cases" className="link">
+            <li className="sub-option" role="menuitem" aria-label="Daily Cases"  tabIndex="0">Cases</li>
           </Link>
-          <Link to="/daily-tested">
-            <li className="sub-option" role="menuitem" aria-label="Daily Tested">Tested</li>
+          <Link to="/daily-tested" className="link">
+            <li className="sub-option" role="menuitem" aria-label="Daily Tested" tabIndex="0">Tested</li>
           </Link>
-          <Link to="/daily-hosp">
-            <li className="sub-option" role="menuitem" aria-label="Daily Hospitalized">Hospitalized</li>
+          <Link to="/daily-hosp" className="link">
+            <li className="sub-option" role="menuitem" aria-label="Daily Hospitalized" tabIndex="0">Hospitalized</li>
           </Link>
-          <Link to="/daily-deaths">
-            <li className="sub-option" role="menuitem" aria-label="Daily Deaths">Deaths</li>
+          <Link to="/daily-deaths" className="link">
+            <li className="sub-option" role="menuitem" aria-label="Daily Deaths" tabIndex="0">Deaths</li>
           </Link>
         </ul>
         <h4 className="option">Vaccine Summary</h4>
-        <ul className="sub-list" value="Vaccine Summary" role="menuitem">
-          <Link to="/vaccine-all">
-            <li className="sub-option" role="menuitem" aria-label="Vaccine Summary All">All Vaccines</li>
+        <ul className="sub-list" value="Vaccine Summary" role="menu">
+          <Link to="/vaccine-all" className="link">
+            <li className="sub-option" role="menuitem" aria-label="Vaccine Summary All" tabIndex="0">All Vaccines</li>
           </Link>
-          <Link to="/vaccine-jnj">
-            <li className="sub-option" role="menuitem" aria-label="Vaccine Summary Johnson & Johnson">Johnson & Johnson Vaccines</li>
+          <Link to="/vaccine-jnj" className="link">
+            <li className="sub-option" role="menuitem" aria-label="Vaccine Summary Johnson & Johnson" tabIndex="0">Johnson & Johnson Vaccines</li>
           </Link>
-          <Link to="/vaccine-pfizer">
-            <li className="sub-option" role="menuitem" aria-label="Vaccine Summary Pfizer">Pfizer Vaccines</li>
+          <Link to="/vaccine-pfizer" className="link">
+            <li className="sub-option" role="menuitem" aria-label="Vaccine Summary Pfizer" tabIndex="0">Pfizer Vaccines</li>
           </Link>
-          <Link to="/vaccine-moderna">
-            <li className="sub-option" role="menuitem" aria-label="Vaccine Summary Moderna">Moderna Vaccines</li>
+          <Link to="/vaccine-moderna" className="link">
+            <li className="sub-option" role="menuitem" aria-label="Vaccine Summary Moderna" tabIndex="0">Moderna Vaccines</li>
           </Link>
-          <Link to="/vaccine-unspecified">
-            <li className="sub-option" role="menuitem" aria-label="Vaccine Summary Unspecified">Unspecified Vaccines</li>
+          <Link to="/vaccine-unspecified" className="link">
+            <li className="sub-option" role="menuitem" aria-label="Vaccine Summary Unspecified" tabIndex="0">Unspecified Vaccines</li>
           </Link>
         </ul>
     </nav>
--- src/components/Sidebar/SidebarMobile.js
+++ src/components/Sidebar/SidebarMobile.js
@@ -8,39 +8,39 @@ const SidebarMobile = () => {
     <Menu className="sidebar-mobile" role="menu" aria-label="Denver Covid-19 Visualization Options">
         <h1 className="title">Colorado Covid-19 Dashboard</h1>
         <Link to="/" role="none">
-          <h4 className="option" role="menuitem">Daily Snapshot</h4>
+          <h4 className="option" role="menuitem" tabIndex="0">Daily Snapshot</h4>
         </Link>
         <h4 className="option">Case Summary</h4>
         <ul className="sub-list" value="Case Summary" role="menuitem">
           <Link to="/daily-cases">
-            <li className="sub-option" role="menuitem" aria-label="Daily Cases">Cases</li>
+            <li className="sub-option" role="menuitem" aria-label="Daily Cases" tabIndex="0">Cases</li>
           </Link>
           <Link to="/daily-tested">
-            <li className="sub-option" role="menuitem" aria-label="Daily Tested">Tested</li>
+            <li className="sub-option" role="menuitem" aria-label="Daily Tested" tabIndex="0">Tested</li>
           </Link>
           <Link to="/daily-hosp">
-            <li className="sub-option" role="menuitem" aria-label="Daily Hospitalized">Hospitalized</li>
+            <li className="sub-option" role="menuitem" aria-label="Daily Hospitalized" tabIndex="0">Hospitalized</li>
           </Link>
           <Link to="/daily-deaths">
-            <li className="sub-option" role="menuitem" aria-label="Daily Deaths">Deaths</li>
+            <li className="sub-option" role="menuitem" aria-label="Daily Deaths" tabIndex="0">Deaths</li>
           </Link>
         </ul>
         <h4 className="option">Vaccine Summary</h4>
         <ul className="sub-list" value="Vaccine Summary" role="menuitem">
           <Link to="/vaccine-all">
-            <li className="sub-option" role="menuitem" aria-label="Vaccine Summary All">All Vaccines</li>
+            <li className="sub-option" role="menuitem" aria-label="Vaccine Summary All" tabIndex="0">All Vaccines</li>
           </Link>
           <Link to="/vaccine-jnj">
-            <li className="sub-option" role="menuitem" aria-label="Vaccine Summary Johnson & Johnson">Johnson & Johnson Vaccines</li>
+            <li className="sub-option" role="menuitem" aria-label="Vaccine Summary Johnson & Johnson" tabIndex="0">Johnson & Johnson Vaccines</li>
           </Link>
           <Link to="/vaccine-pfizer">
-            <li className="sub-option" role="menuitem" aria-label="Vaccine Summary Pfizer">Pfizer Vaccines</li>
+            <li className="sub-option" role="menuitem" aria-label="Vaccine Summary Pfizer" tabIndex="0">Pfizer Vaccines</li>
           </Link>
           <Link to="/vaccine-moderna">
-            <li className="sub-option" role="menuitem" aria-label="Vaccine Summary Moderna">Moderna Vaccines</li>
+            <li className="sub-option" role="menuitem" aria-label="Vaccine Summary Moderna" tabIndex="0">Moderna Vaccines</li>
           </Link>
           <Link to="/vaccine-unspecified">
-            <li className="sub-option" role="menuitem" aria-label="Vaccine Summary Unspecified">Unspecified Vaccines</li>
+            <li className="sub-option" role="menuitem" aria-label="Vaccine Summary Unspecified" tabIndex="0">Unspecified Vaccines</li>
           </Link>
         </ul>
     </Menu>
